50abfd6dfc01cf13d8f374393431dafdcb98f378
darlyo <mgkevin@hotmail.fr>
2012-10-25 12:23:10 +0200

    ajout d'une interface pour communiquer avec le joueur

---
+++ src/Utilisateur.java
@@ -0,0 +1,13 @@
+
+/**
+ * Interface de comunication avec l'utilisateur
+ * @author mainguene kevin marie joris
+ */
+public interface Utilisateur
+{
+	/**
+	 * Recupere la colonne ou a choisie de jouer l'utilisateur
+	 * @return le numero de la colonne
+	 */
+	int choisirColonneJouee ();
+}
