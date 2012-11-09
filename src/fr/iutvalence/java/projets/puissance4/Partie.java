22812dc17ea1334584cf152b8da5d708cd8f0342
jojomar <jojomar26@gmail.com>
2012-11-09 01:19:01 +0100

    Ajout de Checkv2 toujours en cours de réalisation, ( puis a rendre independant du nombre de pion a check )

--- src/fr/iutvalence/java/projets/puissance4/Partie.java
+++ src/fr/iutvalence/java/projets/puissance4/Partie.java
@@ -274,5 +274,62 @@ public class Partie
 
 		return CONTINU;
 	}
-
+	/**
+	 * @param x l'abscisse du pions a checker
+	 * @param y l'ordonnée du pion a checker
+	 * @param nbPions le nombre de pions a aligner pour gagner
+	 * @return le gagnant ou 0  pour continuer ou -1 si le case n'existe pas ( plus ou moins impossible )
+	 */
+	public int checkvictoryv2(int x, int y, int nbPions) 
+	{   
+		int couleur;
+	
+		try{ 
+			couleur = Grille.getCase(x,y);  // Voir interaction avec getCase ( static )
+		 }
+		catch (CaseInexistanteException e)
+		{
+			return -1; // la case n'existe pas 
+		}
+		
+		if ((x+3 < Grille.X_MAX) & (Grille.getCase(x+1,y) == couleur) & (Grille.getCase(x+2,y) == couleur) & (Grille.getCase(x+3,y) == couleur))
+			return couleur;
+		if ((x-1 >-1) & (x+2 < Grille.X_MAX) &(Grille.getCase(x-1,y) == couleur) & (Grille.getCase(x+1,y) == couleur) & (Grille.getCase(x+2,y) == couleur))
+			return couleur;
+		if ((x-2 >-1) & (x+1 < Grille.X_MAX) &(Grille.getCase(x-2,y) == couleur) & (Grille.getCase(x-1,y) == couleur) & (Grille.getCase(x+1,y) == couleur))
+			return couleur;
+		if ((x-3 >-1) &(Grille.getCase(x-3,y) == couleur) & (Grille.getCase(x-2,y) == couleur) & (Grille.getCase(x-1,y) == couleur))
+			return couleur; //ligne horizontale check
+		
+		if ((y < Grille.Y_MAX) & (Grille.getCase(x,y+1) == couleur) & (Grille.getCase(x,y+2) == couleur) & (Grille.getCase(x,y+3) == couleur))
+			return couleur;
+		if ((y-1 >-1) & (y+2 < Grille.Y_MAX) &(Grille.getCase(x,y-1) == couleur) &  (Grille.getCase(x,y+1) == couleur) & (Grille.getCase(x,y+2) == couleur))
+			return couleur;
+		if ((y-1 >-2) & (y+1 < Grille.Y_MAX) &(Grille.getCase(x,y-2) == couleur) &  (Grille.getCase(x,y-1) == couleur) & (Grille.getCase(x,y+1) == couleur))
+			return couleur;
+		if ((y-1 >-3) &(Grille.getCase(x,y-3) == couleur) &  (Grille.getCase(x,y-2) == couleur) & (Grille.getCase(x,y-1) == couleur))
+			return couleur; //ligne verticale check
+		
+		if ((x+3 < Grille.X_MAX) & (y+3 < Grille.Y_MAX) & (Grille.getCase(x+1,y+1)== couleur) & (Grille.getCase(x+2,y+2)== couleur) & (Grille.getCase(x+3,y+3)== couleur))
+			return couleur;
+		if ((x+2 < Grille.X_MAX) & (y+2 < Grille.Y_MAX) & (x-1 > -1) & (y-1 >-1) & (Grille.getCase(x-1,y-1)== couleur) & (Grille.getCase(x+1,y+1)== couleur) & (Grille.getCase(x+2,y+2)== couleur))
+			return couleur;
+		if ((x+1 < Grille.X_MAX) & (y+1 < Grille.Y_MAX) & (x-2 > -1) & (y-2 >-1) & (Grille.getCase(x-2,y-2)== couleur) & (Grille.getCase(x-1,y-1)== couleur) & (Grille.getCase(x+1,y+1)== couleur))
+			return couleur;
+		if ((x-3 > -1) & (y-3 >-1) & (Grille.getCase(x-3,y-3)== couleur) & (Grille.getCase(x-2,y-2)== couleur) & (Grille.getCase(x-1,y-1)== couleur))
+			return couleur; // Diagonnale gauche droite check
+		
+		if ((x-3 > -1) & (y+3 <Grille.Y_MAX) & (this.grille.grille[x-3][y+3] == couleur) & (this.grille.grille[x-2][y+2] == couleur) & (this.grille.grille[x-1][y+1] == couleur))
+			return couleur;
+		
+		
+		
+		
+		
+		
+		return 0;
+		
+	}
 }
+
+
