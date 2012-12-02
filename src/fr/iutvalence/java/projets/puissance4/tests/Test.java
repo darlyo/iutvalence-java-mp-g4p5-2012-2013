b2d386301733c72437b9a2a9358808c6ba053d50
sebastienjean <baz.jean@gmail.com>
2012-12-02 19:36:29 +0100

    Ajout d'un classe de test (test de push)

---
+++ src/fr/iutvalence/java/projets/puissance4/tests/Test.java
@@ -0,0 +1,15 @@
+package fr.iutvalence.java.projets.puissance4.tests;
+
+public class Test
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
